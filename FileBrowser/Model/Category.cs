08b3bf0505e1d79c9af06ada74aec94fcb9e2745
monoscale <bertdesaffel@gmail.com>
2019-01-29 20:36:27 +0100

    initial models + gui testing

---
+++ FileBrowser/Model/Category.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace FileBrowser.Model {
+    [Table("Categories")]
+    public class Category {
+        [Key]
+        public string Name { get; set; }
+
+        public ICollection<Folder> Folders { get; set; }
+        public ICollection<FileExtension> Extensions { get; set; }
+
+        public Category() {
+            Folders = new List<Folder>();
+            Extensions = new List<FileExtension>();
+        }
+
+        public Category(string name) : this(name, new List<Folder>(), new List<FileExtension>()) { }
+
+        public Category(string name, ICollection<Folder> folders, ICollection<FileExtension> extensions) {
+            Name = name;
+            Folders = folders;
+            Extensions = extensions;
+        }
+    }
+}
---
+++ FileBrowser/Model/FileExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FileBrowser.Model {
+    public class FileExtension {
+
+        [Key]
+        public string Extension { get; set; }
+
+        public FileExtension(string extension) {
+            Extension = extension;
+        }
+    }
+}
